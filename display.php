55e94ecdc88c9e9f2a723403bc3130f2185779e9
Oka <tonkotsu6218@gmail.com>
2019-06-15 14:58:27 +0900

    [add] show books data

--- display.php
+++ display.php
@@ -7,18 +7,17 @@ try  {
   $stmt_books = $dbh->query('SELECT * FROM books');
   $stmt_categ = $dbh->query('SELECT * FROM categories');
   $stmt_author = $dbh->query('SELECT * FROM authors');
-  $row_author = $stmt_author->fetch(PDO::FETCH_ASSOC);
 ?>
 
 <!DOCTYPE html>
 <html lang="ja-JP">
 <head>
   <meta charset="UTF-8">
-  <title>一覧</title>
+  <title>本一覧</title>
 </head>
 <body>
-  <h1>一覧</h1>
-    <table>
+  <h1>本一覧</h1>
+    <table border=1>
     <tr>
       <th>カテゴリ</th>
       <th>書籍名(ISBN)</th>
@@ -27,24 +26,25 @@ try  {
       <th>タグ</th>
     </tr>
     <?php 
-    $category_value = '';
-    $author_value = '';
     while ($row_books = $stmt_books->fetch(PDO::FETCH_ASSOC)) {
       while($row_categ = $stmt_categ->fetch(PDO::FETCH_ASSOC)) {
-        var_dump($row_books);
-        var_dump($row_categ);
-        if($row_categ['id'] == $stmt_categ['category_id']){
+        if($row_categ['id'] == $row_books['category_id']){
           $category_value = $row_categ['name'];
           break;
         }
       }
-    // var_dump($category_value);
+      while($row_author = $stmt_author->fetch(PDO::FETCH_ASSOC)) {
+        if($row_author['id'] == $row_books['author_id']){
+          $author_value = $row_author['name'];
+          break;
+        }
+      }
     ?>
     <tr>
-      <td></td>
-      <td></td>
-      <td></td>
-      <td></td>
+      <td><?=$category_value?></td>
+      <td><?=$row_books['name']?></br>(<?=$row_books['isbn']?>)</td>
+      <td><?=$author_value?></td>
+      <td><?=$row_books['price']?></br>円</td>
       <td></td>
     </tr>
     <?php } ?>
